7434c99d58113d4b928ce9c1a2d9926c85b3ad49
Vineet Rajput <134199882+Vineetrajput09@users.noreply.github.com>
2025-08-19 10:30:45 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const port =3000
+const web  =require('./routes/web')
+const connectDB = require('./db/connectDB')
+const fileUpload = require('express-fileupload')
+const cors = require("cors");
+const cookieParser = require('cookie-parser')
+
+require("dotenv").config()
+
+
+
+
+app.use(
+    cors({
+      origin: "http://localhost:5173", // Your frontend domain
+      credentials: true, // allow credentials (cookies)
+    })
+  );
+
+
+app.use(cookieParser())
+//image uploadd
+app.use(fileUpload({
+    useTempFiles : true,
+   
+}));
+
+
+//connectDB
+connectDB()
+
+app.use(express.json())
+
+
+
+
+
+
+
+app.use('/api',web) //localhost:3000/api/
+app.listen(process.env.PORT,console.log('server start localhost:8000'))
---
+++ db/connectDB.js
@@ -0,0 +1,15 @@
+
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  return mongoose.connect(process.env.LIVE_URL)
+
+    .then(() => {
+      console.log("Database Connection successful");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+module.exports = connectDB;
